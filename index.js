691f935db24b3035c7da7790743f561612f0759c
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2022-07-10 23:00:29 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,128 @@
+function reiniciar() {
+  for (let i = 0; i < botoes.length; i++) {
+    botoes[i].innerText = "";
+  }
+  atualizaStatus("");
+}
+
+function atualizaStatus(texto) {
+  let status = document.getElementById("status");
+  status.innerText = texto;
+}
+
+function verificaVitoriaHorizontal() {
+  if (
+    botoes[0].innerText == vez &&
+    botoes[1].innerText == vez &&
+    botoes[2].innerText == vez
+  ) {
+    return true;
+  } else if (
+    botoes[3].innerText == vez &&
+    botoes[4].innerText == vez &&
+    botoes[5].innerText == vez
+  ) {
+    return true;
+  } else if (
+    botoes[6].innerText == vez &&
+    botoes[7].innerText == vez &&
+    botoes[8].innerText == vez
+  ) {
+    return true;
+  }
+}
+
+function verificaVitoriaVertical() {
+  if (
+    botoes[0].innerText == vez &&
+    botoes[3].innerText == vez &&
+    botoes[6].innerText == vez
+  ) {
+    return true;
+  } else if (
+    botoes[1].innerText == vez &&
+    botoes[4].innerText == vez &&
+    botoes[7].innerText == vez
+  ) {
+    return true;
+  } else if (
+    botoes[2].innerText == vez &&
+    botoes[5].innerText == vez &&
+    botoes[8].innerText == vez
+  ) {
+    return true;
+  }
+}
+
+function verificaVitoriaDiagonal() {
+  if (
+    botoes[0].innerText == vez &&
+    botoes[4].innerText == vez &&
+    botoes[8].innerText == vez
+  ) {
+    return true;
+  } else if (
+    botoes[2].innerText == vez &&
+    botoes[4].innerText == vez &&
+    botoes[6].innerText == vez
+  ) {
+    return true;
+  }
+}
+
+function verificaDeuVelha() {
+  let deuVelha = true; // inicia como true mas recebe false se ainda tiver espaços vazios
+  for (let i = 0; i < botoes.length; i++) {
+    if (botoes[i].innerText == "") {
+      deuVelha = false;
+    }
+  }
+  return deuVelha;
+}
+
+function verificaVitoria() {
+  //todas as casas horizontais(3 verificacoes)
+  //012//345//678
+  //horizontal
+  let vitoriaH = verificaVitoriaHorizontal();
+  let vitoriaV = verificaVitoriaVertical();
+  let vitoriaD = verificaVitoriaDiagonal();
+  if (vitoriaH == true || vitoriaV == true || vitoriaD == true) {
+    atualizaStatus(vez + " GANHOU!");
+  } else {
+    let velha = verificaDeuVelha();
+    if (velha == true) {
+      atualizaStatus("Deu velha!");
+    }
+  }
+}
+
+function trocarVez() {
+  if (vez == "X") {
+    vez = "O";
+  } else if (vez == "O") {
+    vez = "X";
+  }
+}
+
+function gerarVetorDeBotoes() {
+  for (let i = 0; i < 9; i++) {
+    let botaoElemento = document.getElementById(i);
+    botoes.push(botaoElemento);
+  }
+}
+
+function botao(numeroDoBotao) {
+  if (botoes[numeroDoBotao].innerText == "") {
+    botoes[numeroDoBotao].innerText = vez;
+    verificaVitoria();
+    trocarVez();
+  }
+}
+
+var botoes = [];
+gerarVetorDeBotoes();
+
+var vez = "X";
+
+console.log(botoes);
